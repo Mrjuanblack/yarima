90458c2028664cbf058338e273681c1ee66b31c5
Juan Jose Vera Briceño <juancho_2ce@hotmail.com>
2025-10-07 13:12:42 -0500

    Added social links to footer. Added loc info on footer. Added legal info on footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,24 +2,31 @@ import Container from "./Container"
 import { FaInstagram } from "react-icons/fa";
 import { FaFacebookSquare } from "react-icons/fa";
 import { FaYoutube } from "react-icons/fa";
-import { AiFillTikTok } from "react-icons/ai";
+// import { AiFillTikTok } from "react-icons/ai";
 
 
 const Footer: React.FC = () => {
     const socialMediaSize = "w-5 h-auto";
     return (
-        <footer className="bg-gradient-to-br from-theme-background-dark-950 via-theme-background-dark-950/95 to-theme-background-dark-950 py-16 px-6 lg:px-8 text-theme-background-200">
+        <footer className="bg-gradient-to-br from-theme-background-dark-950 via-theme-background-dark-950/95 to-theme-background-dark-950 pt-16 pb-8 px-6 lg:px-8 text-theme-background-200">
             <Container>
                 <div className="flex flex-col gap-4">
                     <div className="flex justify-center items-center">
                         <img src="logo-full-blanco.png" alt="Yarima" className="w-auto h-40" />
                     </div>
                     <div className="flex justify-center items-center gap-4">
-                        <FaInstagram className={socialMediaSize} />
-                        <FaFacebookSquare className={socialMediaSize} />
-                        <FaYoutube className={socialMediaSize} />
-                        <AiFillTikTok className={socialMediaSize} />
+                        <FaInstagram className={`${socialMediaSize} cursor-pointer`} onClick={() => window.open("https://www.instagram.com/yarima.topocoro", "_blank")} />
+                        <FaFacebookSquare className={`${socialMediaSize} cursor-pointer`} onClick={() => window.open("https://www.facebook.com/share/17NMsPpvfX/?mibextid=wwXIfr", "_blank")} />
+                        <FaYoutube className={`${socialMediaSize} cursor-pointer`} onClick={() => window.open("https://www.youtube.com/@YarimaResort", "_blank")} />
+                        {/* <AiFillTikTok className={`${socialMediaSize} cursor-pointer`} onClick={() => window.open("https://www.tiktok.com/@yarimaresort", "_blank")} /> */}
                     </div>
+                    <div className="my-4">
+                        {/* <p className="text-center text-white mb-2"></p> */}
+                        <p className="text-center text-white text-base/2"><span className="font-medium mr-1">Sala de Experiencias: </span>Km 1.4, vía Ruitoque Bajo - Acapulco.</p>
+                        <p className="text-center text-white">Floridablanca, Santander</p>
+                    </div>
+                    <p className="text-center text-white text-sm/1">© {new Date().getFullYear()} Yarima Resort & Club de Playa. Todos los derechos reservados.</p>
+                    <p className="text-center text-white text-sm/1">Desarrollado por <a href="https://mimmers.com" target="_blank" rel="noopener noreferrer" className="text-theme-gold">MIMMERS</a></p>
                 </div>
             </Container>
         </footer>
