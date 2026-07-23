5e07d73a9fbbebd07bbeaa2c2dc1823b4a833575
Juan Jose Vera Briceño <juancho_2ce@hotmail.com>
2026-07-23 09:38:54 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  async redirects() {
+    return [
+      {
+        source: '/obra',
+        destination: 'https://www.instagram.com/reels/DbEc3sRxYKM/',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
